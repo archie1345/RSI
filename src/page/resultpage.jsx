54d0f498ead1d6764ab0f21aea7a05a211710d97
satrio akbar <satrioakbararadhana@gmail.com>
2025-06-10 13:44:11 +0700

    FIX UI

--- src/page/resultpage.jsx
+++ src/page/resultpage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { supabase } from '../supabaseClient';
-import { calorieApi } from '../api/calorieAPI'; // ✅ Tambahkan ini
+import { calorieApi } from '../api/calorieAPI';
 import './Caloriecalc.css';
 
 function ResultPage() {
